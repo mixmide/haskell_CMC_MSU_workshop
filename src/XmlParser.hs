29747c5d876224cf7751c2f4d9111623913b490d
Dmitry <zverev_03@inbox.ru>
2025-04-09 14:12:34 +0300

    Delete src/XmlParser.hs

--- src/XmlParser.hs
+++
@@ -1,7 +0,0 @@
-module XmlParser where
-
-import XMLTypes
-
--- Парсит строку в XML-структуру и возвращает либо строку с ошибкой или XMLNode
-parseXml :: String -> Either String XMLNode
-parseXml = "help"  -- здесь будет парсер
